9633175fb2e39cce791d62c4b8fe4b7c640ce1bf
Allison Durkan <azulad7@bu.edu>
2016-10-22 19:54:24 -0400

    wtf

---
+++ HoloBackSpeaker/Assets/pingTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pingTest : MonoBehaviour
+{
+
+    public string url = "https://arc.applause.com/wp-content/uploads/2015/04/build_alex_kipman-1024x515.jpg";
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+
+    /*IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }*/
+}
